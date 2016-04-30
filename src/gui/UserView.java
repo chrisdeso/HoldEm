0e3f6c8d6033b16bd0ca87d48067ed576668b315
Chris de Sousa <cdesousa89@gmail.com>
2016-04-30 00:07:26 -0700

    Added buttons

--- src/gui/UserView.java
+++ src/gui/UserView.java
@@ -4,74 +4,74 @@ import components.Deck;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Class that represents the GUI
  * @author Christopher de Sousa, Sean Clements
  */
-public class UserView extends JFrame {
+public class UserView extends JFrame implements ActionListener{
 
     private static Dimension userScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-    private JFrame PokerTable = new JFrame();
-    private JButton restartGameButton = new JButton();
-    private JButton callButton = new JButton();
-    private JButton raiseButton = new JButton();
-    private JButton checkButton = new JButton();
-    private JButton foldButton = new JButton();
-    private JButton dealButton = new JButton();
-    private JTextArea betBox = new JTextArea();
-    //private Deck tableCards = new Deck();
+    private JPanel PokerTable = new JPanel();
+    private JPanel Buttons = new JPanel();
+
+    private JButton restartGameButton;
+    private JButton callButton;
+    private JButton raiseButton;
+    private JButton checkButton;
+    private JButton foldButton;
+    private JButton dealButton;
+    private JTextArea betBox;
+
+    private Deck tableCards;
 
     // Populate view
     public UserView () {
 
-        PokerTable.setTitle("Hold'Em Alpha");
-        PokerTable.setSize(1000, 600);
-        PokerTable.setVisible(true);
-        // Gets user screen size and populates HoldEm window in the middle
-        PokerTable.setLocation((userScreenSize.width / 2) - (PokerTable.getSize().width / 2),
-                userScreenSize.height / 2 - PokerTable.getSize().height / 2);
-        PokerTable.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Container tablePane = getContentPane();
+        tablePane.setLayout(null);
+        tablePane.setBackground(Color.DARK_GRAY);
 
-        /*//Buttons Panel
-        JPanel buttons = new JPanel();
-        buttons.add(restartGameButton);
-*/
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Hold'Em Alpha");
+        setSize(800, 750);
+        setLocation((userScreenSize.width / 2) - (getSize().width / 2), userScreenSize.height / 2 - getSize().height / 2);
+        setResizable(false);
+
+        restartGameButton = new JButton("Restart");
+        restartGameButton.setBounds(50, 600, 75, 25);
+        //restartGameButton.addActionListener(this);
+        tablePane.add(restartGameButton);
+
+        callButton = new JButton("Call");
+        callButton.setBounds(150, 600, 75, 25);
+        tablePane.add(callButton);
+
+        raiseButton = new JButton("Raise");
+        raiseButton.setBounds(250, 600, 75, 25);
+        tablePane.add(raiseButton);
+
+        checkButton = new JButton("Check");
+        checkButton.setBounds(350, 600, 75, 25);
+        tablePane.add(checkButton);
+
+        foldButton = new JButton("Fold");
+        foldButton.setBounds(450, 600, 75, 25);
+        tablePane.add(foldButton);
+
     }
 
-    private JButton restartGameButton() {
-        restartGameButton.setText("Restart Game");
-        restartGameButton.setVisible(false);
-        restartGameButton.setForeground(Color.WHITE);
-        restartGameButton.setBounds(30, 50, 50, 50);
-        restartGameButton.setOpaque(false);
-        restartGameButton.setContentAreaFilled(false);
-        return restartGameButton;
-    }
-
-    private JButton callButton() {
-        return callButton;
-    }
-
-    private JButton raiseButton() {
-        return raiseButton;
-    }
-
-    private JButton checkButton() {
-        return checkButton;
-    }
-
-    private JButton foldButton() {
-        return foldButton;
-    }
-
-    private JButton dealButton() {
-        return dealButton;
-    }
     // Main method
     public static void main(String[] args) {
         new UserView();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
 

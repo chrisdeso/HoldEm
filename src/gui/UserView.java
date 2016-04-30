5d37d4f5d15001cc3b71b87b9eefcd684a300e99
Chris de Sousa <cdesousa89@gmail.com>
2016-04-30 15:44:42 -0700

    Button grid layout

--- src/gui/UserView.java
+++ src/gui/UserView.java
@@ -14,8 +14,8 @@ public class UserView extends JFrame implements ActionListener{
 
     private static Dimension userScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-    private JPanel PokerTable = new JPanel();
-    private JPanel Buttons = new JPanel();
+    private JPanel PokerTable;
+    private JPanel Buttons;
 
     private JButton restartGameButton;
     private JButton callButton;
@@ -30,10 +30,12 @@ public class UserView extends JFrame implements ActionListener{
     // Populate view
     public UserView () {
 
+        // Create Pane (Window)
         Container tablePane = getContentPane();
-        tablePane.setLayout(null);
+        tablePane.setLayout(new BorderLayout());
         tablePane.setBackground(Color.DARK_GRAY);
 
+        // Pane attributes
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Hold'Em Alpha");
@@ -41,26 +43,50 @@ public class UserView extends JFrame implements ActionListener{
         setLocation((userScreenSize.width / 2) - (getSize().width / 2), userScreenSize.height / 2 - getSize().height / 2);
         setResizable(false);
 
-        restartGameButton = new JButton("Restart");
-        restartGameButton.setBounds(50, 600, 75, 25);
-        //restartGameButton.addActionListener(this);
-        tablePane.add(restartGameButton);
+        /*
+        * JPanel for cards on table
+        * */
 
+
+        // Add panel at bottom of screen for buttons
+        Buttons = new JPanel();
+        Buttons.setLayout(new GridLayout());
+
+        // Add Buttons panel to window (tablepane)
+        tablePane.add(Buttons, BorderLayout.SOUTH);
+
+        /*
+        * Restart Button Logic
+        *
+        * */
+        restartGameButton = new JButton("Restart");
+        restartGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent restart) {
+
+            }
+        });
+        Buttons.add(restartGameButton);
+
+        // Call Button attributes & add to Buttons panel
         callButton = new JButton("Call");
         callButton.setBounds(150, 600, 75, 25);
-        tablePane.add(callButton);
+        Buttons.add(callButton);
 
+        // Raise Button attributes & add to Buttons panel
         raiseButton = new JButton("Raise");
         raiseButton.setBounds(250, 600, 75, 25);
-        tablePane.add(raiseButton);
+        Buttons.add(raiseButton);
 
+        // Check Button attributes & add to Buttons panel
         checkButton = new JButton("Check");
         checkButton.setBounds(350, 600, 75, 25);
-        tablePane.add(checkButton);
+        Buttons.add(checkButton);
 
+        // Fold Button attributes & add to Buttons panel
         foldButton = new JButton("Fold");
         foldButton.setBounds(450, 600, 75, 25);
-        tablePane.add(foldButton);
+        Buttons.add(foldButton);
 
     }
 
